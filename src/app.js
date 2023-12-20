1bb596f8c160a7351b4ea171939e1614770df5f7
lubhub612 <lundhakgithub@gmail.com>
2023-12-20 23:24:22 +0530

    today new commit 3

--- src/app.js
+++ src/app.js
@@ -84,7 +84,7 @@ const ethereumClient = new EthereumClient(wagmiConfig, chains)  */
 const projectId = '5617cf9a0fa15b77934dc64c33693c27'
 
 const { chains, publicClient } = configureChains(
-  [mainnet, polygon, optimism, arbitrum, base, zora],
+  [ polygon, optimism, arbitrum, base, zora],
   [
    // alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
     publicProvider()
